c8ae5a6b8816cc5e05ba0840ff5c8bb066466e96
Paul Herz <phrz@me.com>
2016-10-18 14:38:05 -0500

    Initial commit

---
+++ src/NewtonForm.cpp
@@ -0,0 +1,13 @@
+//
+//  NewtonForm.cpp
+//  HPSCProject3
+//
+//  Created by Paul Herz on 10/18/16.
+//  Copyright © 2016 Paul Herz. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace PH;
+
+void Newton_basis(Matrix& xnodes, int n, double x)
---
+++ src/test_NewtonForm.cpp
@@ -0,0 +1,15 @@
+//
+//  test_NewtonForm.cpp
+//  HPSCProject3
+//
+//  Created by Paul Herz on 10/18/16.
+//  Copyright © 2016 Paul Herz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
